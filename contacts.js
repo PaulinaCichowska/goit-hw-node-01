e8948b34f75aeb654b71fb7f58e31c0976722127
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-12-20 00:16:15 +0100

    update

---
+++ contacts.js
@@ -0,0 +1,22 @@
+function listContacts() {
+    // ...twój kod
+}
+
+function getContactById(contactId) {
+    // ...twój kod
+}
+
+function removeContact(contactId) {
+    // ...twój kod
+}
+
+function addContact(name, email, phone) {
+    // ...twój kod
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
