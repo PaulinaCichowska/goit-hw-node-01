0db7fb3774d202f0dd85acce604279df08d8b33f
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-12-19 23:22:07 +0100

    add node modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("xd")
+import redline from "redline"
+import { Command } from "commander"
+import * as yargs from 'yargs'
+
+
+const argv = yargs.argv
+
+const program = new Command();
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: refaktor
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case "list":
+            // ...
+            break;
+
+        case "get":
+            // ... id
+            break;
+
+        case "add":
+            // ... name email phone
+            break;
+
+        case "remove":
+            // ... id
+            break;
+
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+invokeAction(argv);
